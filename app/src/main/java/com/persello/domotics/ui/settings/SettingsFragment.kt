a235eca31cfaaa928954686af7a0ef8a7774a067
mp269437 <maxence25000@gmail.com>
2025-03-04 13:44:30 +0100

    Added logout button and process to the app, destroying the token in storage, the app checks if the token is present onResume, onCreate, and on navigation tab change

--- app/src/main/java/com/persello/domotics/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/persello/domotics/ui/settings/SettingsFragment.kt
@@ -1,5 +1,6 @@
 package com.persello.domotics.ui.settings
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,35 +9,53 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.persello.domotics.databinding.FragmentSettingsBinding
+import com.persello.domotics.storage.auth.TokenStorage
+import com.persello.domotics.ui.auth.LoginActivity
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
 
 class SettingsFragment : Fragment() {
 
-    private var _binding: FragmentSettingsBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    private var _binding: FragmentSettingsBinding? = null;
+    private val binding get() = _binding!!;
+    private lateinit var tokenStorage: TokenStorage;
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val settingsViewModel =
-            ViewModelProvider(this).get(SettingsViewModel::class.java)
+        val settingsViewModel = ViewModelProvider(this).get(SettingsViewModel::class.java);
 
-        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        _binding = FragmentSettingsBinding.inflate(inflater, container, false);
+        val root: View = binding.root;
 
-        val textView: TextView = binding.textSettings
+        val textView: TextView = binding.textSettings;
         settingsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+            textView.text = it;
         }
+
+        tokenStorage = TokenStorage(requireContext());
+
+        binding.btnSettingsLogout.setOnClickListener {
+            logoutUser()
+        }
+
         return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun logoutUser() {
+        MainScope().launch {
+            tokenStorage.clearToken();
+
+            requireActivity().run {
+                startActivity(Intent(this, LoginActivity::class.java))
+            }
+        }
     }
 }
