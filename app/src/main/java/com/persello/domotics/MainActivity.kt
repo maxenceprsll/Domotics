cb7eb17756fa1995879259e689d2e107dd8e1513
mp269437 <maxence25000@gmail.com>
2025-03-04 10:27:59 +0100

    changed repository name to domotics, changed auth check to be in mainActivity, changed androidManifest accordingly

--- app/src/main/java/com/persello/domotics/MainActivity.kt
+++ app/src/main/java/com/persello/domotics/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.persello.domotics
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
@@ -8,28 +9,47 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.persello.domotics.databinding.ActivityMainBinding
+import com.persello.domotics.storage.auth.TokenStorage
+import com.persello.domotics.ui.auth.LoginActivity
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding;
+    private lateinit var tokenStorage: TokenStorage;
+    private val mainScope = MainScope();
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        super.onCreate(savedInstanceState);
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        binding = ActivityMainBinding.inflate(layoutInflater);
+        setContentView(binding.root);
 
-        val navView: BottomNavigationView = binding.navView
+        val navView: BottomNavigationView = binding.navView;
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        val navController = findNavController(R.id.nav_host_fragment_activity_main);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_favorites, R.id.navigation_devices, R.id.navigation_settings
             )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        );
+        setupActionBarWithNavController(navController, appBarConfiguration);
+        navView.setupWithNavController(navController);
+
+        tokenStorage = TokenStorage(this);
+
+        mainScope.launch {
+            if(tokenStorage.read().isEmpty()) {
+                goToLogin();
+            }
+        }
+    }
+
+    private fun goToLogin() {
+        val intent = Intent(this, LoginActivity::class.java);
+        startActivity(intent);
     }
 }
